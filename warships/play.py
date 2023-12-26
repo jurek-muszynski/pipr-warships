05da3f06738bd87a1c373c3b539c727729ea30bf
jmuszyns <j.muszynski@stud.elka.pw.edu.pl>
2023-12-26 23:45:20 +0100

    instructions + invalid board size input capturing

--- warships/play.py
+++ warships/play.py
@@ -17,13 +17,36 @@ def start():
     print("\nLet the game begin!")
     sleep(1)
     system("clear")
-    board_size = input("Enter the size of your board: ")
-    game = Game(int(board_size))
+    while True:
+        try:
+            board_size = input("Enter the size of your board: ")
+            game = Game(int(board_size))
+            break
+        except ValueError:
+            print("Invalid board size")
     game.play()
 
 
 def instructions():
+    system("clear")
     print("Instructions: ")
+    sleep(1)
+    print("\nBOARD PREP\n")
+    print("1 > Choose the size of your board e.g. (2)")
+    print(
+        "2 > Use the arrow keys to place your warships according to your liking")
+    sleep(1)
+    print("\nGAME ON\n")
+    print("1 > Take turns with the AI on hitting the opponent's ships")
+    print(
+        "2 > Enter your hits in the following format e.g. A0 (column A row 0)")
+    print(
+        "3 > x will be displayed in the chosen coordinates, if the hit was successful")
+    print(
+        "4 > # will be displayed in the chosen coordinates, if the hit a miss\n")
+    sleep(1)
+    input("Press any key to continue: ")
+    system("clear")
 
 
 def main():
@@ -34,6 +57,7 @@ def main():
             case "1":
                 start()
             case "2":
+                sleep(1)
                 instructions()
             case "3":
                 sleep(1)
